b418c4903693f09927fd259cdbc18ae00d224e92
alpcagatay <alpcbatuhan@gmail.com>
2024-08-27 21:01:27 +0300

    first commit

---
+++ Day 1/helloworld.py
@@ -0,0 +1,30 @@
+print('Hello World')
+# Strings : written in double quote. 
+
+# \n means next line
+print("Hello World! \n Hello World")
+
+# + concatenates strings
+print ("Hello" + "Alp") # HelloAlp
+print ("Hello" + " "+  "Alp") # Hello Alp
+
+# there shall not be any indentation before the code. 
+# read the errors that you get
+
+# cmd + shift + 7 --> automatically comment
+print("What is your name?")
+# get the input of the user - identical to print function
+input("What is your name?")
+# prints the user's input
+print("Hello " + input("What is your name?") + "!" )
+
+#we can assign the name by using =. name would be a variable. it can be changed
+name = input("What is your name?")
+print(name)
+
+name = "Alp"
+print(name)
+
+name = "Berk"
+print(name)
+
---
+++ Day 1/variables.py
@@ -0,0 +1,16 @@
+# to print int and string in the same line use , instead of +
+name_long = input("What is your name")
+print(len(name_long), "alp")
+
+
+print(len(input("What is your name?")))
+
+username = input("What is your name?")
+length = len(username)
+print(length)
+
+#variable naming
+# make your code readable, use words that would make sense
+# to seperate words use _
+# do not use keywords to name your variables
+
