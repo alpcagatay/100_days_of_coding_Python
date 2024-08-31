eb0a8083dce38654e38038d2dd14189b652ffe25
alpcagatay <alpcbatuhan@gmail.com>
2024-08-31 16:49:59 +0300

    day 6 completed

---
+++ Day 6/indentation.py
@@ -0,0 +1,26 @@
+# Functions
+# name of the function followed by paranthesis
+
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+# creating function
+# def name_of_the_function():
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+# call the function
+my_function()
+
+# def my_function():
+    # Do This
+    # Then do this
+    # Finally do this
+
+# indentation is important. code works according to indentation. 
+
+# you can use tabs or spaces to indent
+# tab = 4 spaces
---
+++ Day 6/while_loop.py
@@ -0,0 +1,81 @@
+# for: look in list and use range
+# while something_is_true: do that
+
+number_of_hurdles = 6
+while number_of_hurdles>0:
+    number_of_hurdles -= 1
+    print(number_of_hurdles)
+
+# while something_is_true:
+    # do this
+    # Then do this
+    # Then do this
+
+# for loop when you need to iterate
+# while until something is satisfied. do something repeatedly
+
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+
+    
+# while at_goal() == False:
+#     if front_is_clear() == True:
+#         move()
+#     elif wall_in_front() == True:
+#         jump()
+
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+    
+# while not at_goal():
+#     if wall_in_front:
+#         jump()
+#     else:
+#         move()
+
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# while front_is_clear():
+#     move()
+# turn_left()
+    
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+ 
