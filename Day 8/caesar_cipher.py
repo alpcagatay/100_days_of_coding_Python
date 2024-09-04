2e538341a3217f83adf9f5b77374e30729e5507d
alpcagatay <alpcbatuhan@gmail.com>
2024-09-04 21:18:04 +0300

    day 8 project part 2 completed

--- Day 8/caesar_cipher.py
+++ Day 8/caesar_cipher.py
@@ -13,31 +13,31 @@ shift = int(input("Type the shift number:\n"))
 
 
 
-def encrypt(original_text,shift_amount):
+# def encrypt(original_text,shift_amount):
 
 
 
-# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
-#  by the shift amount and print the encrypted text.
-    cipher_text = ""
-    for char in original_text:
-        new_char = alphabet.index(char) + int(shift_amount)
-        if new_char > 25:
-            new_char -= 25
-        cipher_text += alphabet[new_char]
-    print(f"Here is the encoded result {cipher_text}")
+# # TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
+# #  by the shift amount and print the encrypted text.
+#     cipher_text = ""
+#     for char in original_text:
+#         new_char = alphabet.index(char) + int(shift_amount)
+#         if new_char > 25:
+#             new_char -= 25
+#         cipher_text += alphabet[new_char]
+#     print(f"Here is the encoded result {cipher_text}")
 #if direction == "encode":
 #    encrypt(text,shift)
 
-def decrypt(original_text, shift_amount):
-    decrypted_text = ""
+# def decrypt(original_text, shift_amount):
+#     decrypted_text = ""
 
-    for char in original_text:
-        new_char = alphabet.index(char) - int(shift_amount)
-        if new_char > 25:
-            new_char -= 25
-        decrypted_text += alphabet[new_char]
-    print(f"Here is the decoded result {decrypted_text}")
+#     for char in original_text:
+#         new_char = alphabet.index(char) - int(shift_amount)
+#         if new_char > 25:
+#             new_char -= 25
+#         decrypted_text += alphabet[new_char]
+#     print(f"Here is the decoded result {decrypted_text}")
 
 # def ceasar(direction,text,shift):
 
@@ -45,6 +45,18 @@ def decrypt(original_text, shift_amount):
 #         encrypt(text,shift)
 #     elif direction == "decode":
 #         decrypt(text,shift)
+
+def ceasar (original_text, shift_amount,encode_or_decode):
+    output_text = ""
+    for letter in original_text:
+        if encode_or_decode =="decode":
+            shift_amount*= -1
+        shifted_position = alphabet.index(letter) + shift_amount
+        shifted_position %= len(alphabet)
+        output_text += alphabet[shifted_position]
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+ceasar(original_text=text,shift_amount=shift,encode_or_decode=direction)
 
 
 # TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
